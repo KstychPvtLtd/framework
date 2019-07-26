2b843ba6219991ae9baf411734edf763e2b39bf9
Siddharth Upmanyu <siddharth@kstych.com>
2019-07-26 13:02:10 +0530

    updates

--- kstych.sh
+++ kstych.sh
@@ -8,12 +8,7 @@ ARG4=${4:-127.0.0.1}
 APIKEY=`cat /dev/urandom |head -c 32 | base64`
 
 if [[ "$(docker images -q kstych/framework 2> /dev/null)" == "" ]]; then
-  if [ -f framework.tar ]; then
-    echo "Importing framework.tar"
-    docker load -i framework.tar
-  else
-    docker pull kstych/framework
-  fi
+  docker pull kstych/framework
 fi
 
 mkdir -p data/custom
@@ -26,60 +21,60 @@ echo ".env" >> data/custom/.gitignore
 
 if [ ! -f data/custom/.env ]; then
 
-  echo "APP_ENV=local
-  APP_DEBUG=false
-  APP_KEY=base64:$APIKEY
+echo "APP_ENV=local
+APP_DEBUG=false
+APP_KEY=base64:$APIKEY
 
-  APP_URL=http://$ARG2
-  APP_NAME=App
+APP_URL=http://$ARG2
+APP_NAME=App
 
-  APP_ADMIN_DEBUG=true
-  app_title=Framework
-  app_ip=$ARG4
-  app_masterpassword=yb9738z
+APP_ADMIN_DEBUG=true
+app_title=Framework
+app_ip=$ARG4
+app_masterpassword=yb9738z
 
-  asterisk_domain=$ARG3
-  asterisk_slaves=$ARG4:1001:21000:1:240
-  asterisk_manager=$ARG4
-  asterisk_extensions=\"31332,_62XXXX!\"
-  domain_alisas=
+asterisk_domain=$ARG3
+asterisk_slaves=$ARG4:1001:21000:1:240
+asterisk_manager=$ARG4
+asterisk_extensions=\"31332,_62XXXX!\"
+domain_alisas=
 
-  LOG_CHANNEL=stack
+LOG_CHANNEL=stack
 
-  APP_Multiple_Logins=yes
-  kDialer_keeplocalconf=0
+APP_Multiple_Logins=yes
+kDialer_keeplocalconf=0
 
-  DB_CONNECTION=mysql
-  DB_HOST=localhost
-  DB_PORT=3306
-  DB_DATABASE=app_test
-  DB_USERNAME=root
-  DB_PASSWORD=yb9738z
+DB_CONNECTION=mysql
+DB_HOST=localhost
+DB_PORT=3306
+DB_DATABASE=app_test
+DB_USERNAME=root
+DB_PASSWORD=yb9738z
 
-  BROADCAST_DRIVER=log
-  CACHE_DRIVER=database
-  SESSION_DRIVER=database
-  SESSION_LIFETIME=43200
-  QUEUE_CONNECTION=database
+BROADCAST_DRIVER=log
+CACHE_DRIVER=database
+SESSION_DRIVER=database
+SESSION_LIFETIME=43200
+QUEUE_CONNECTION=database
 
-  MAIL_DRIVER=log
-  MAIL_HOST=email-smtp.us-east-1.amazonaws.com
-  MAIL_PORT=587
-  MAIL_USERNAME=
-  MAIL_PASSWORD=
-  MAIL_FROM_ADDRESS=siddharth@kstych.com
-  MAIL_FROM_NAME=Framework
-  app_developer=siddharth@kstych.com
+MAIL_DRIVER=log
+MAIL_HOST=email-smtp.us-east-1.amazonaws.com
+MAIL_PORT=587
+MAIL_USERNAME=
+MAIL_PASSWORD=
+MAIL_FROM_ADDRESS=siddharth@kstych.com
+MAIL_FROM_NAME=Framework
+app_developer=siddharth@kstych.com
 
-  AWS_ACCESS_KEY_ID=
-  AWS_SECRET_ACCESS_KEY=
-  AWS_DEFAULT_REGION=us-east-1
-  AWS_BUCKET=bucket-name
-  AWS_URL=
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_DEFAULT_REGION=us-east-1
+AWS_BUCKET=bucket-name
+AWS_URL=
 
-  FILESYSTEM_DRIVER=local
-  FILESYSTEM_CLOUD=s3
-  " > data/custom/.env
+FILESYSTEM_DRIVER=local
+FILESYSTEM_CLOUD=s3
+" > data/custom/.env
 
 fi
 
